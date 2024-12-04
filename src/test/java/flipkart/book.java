3af8e700c7e8eca8d598dfb7666fb91a33faaeb7
vishaliselvakumar <selvakuamrselvi14@gmail.com>
2024-12-04 12:31:33 +0530

    commit first

---
+++ src/test/java/flipkart/book.java
@@ -0,0 +1,33 @@
+package flipkart;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class book {
+	public WebDriver driver;
+	
+	public void launch() {
+     driver=new ChromeDriver();
+     Actions a=new Actions(driver);
+    JavascriptExecutor js = (JavascriptExecutor)driver;
+    
+	WebDriverManager.chromedriver().setup();
+	
+	driver.get("www.flipkart.com");
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+	WebElement Beauty=driver.findElement(By.xpath("//span[text()='Beauty, Toys & More']"));
+    a.moveToElement(Beauty).build().perform();
+	WebElement book=driver.findElement(By.xpath("//a[@class='_1BJVlg _11MZbx' and text()='Books']"));
+	js.executeScript("arguments[0].click()",book);
+	
+
+}
+}
---
+++ target/test-classes/book.feature
@@ -0,0 +1,9 @@
+Feature: Flipkart Application
+
+ 
+Scenario: Testing the book fucnctionality
+    Given user is on the login page
+    When user click beauty products
+    Then user select the book button option
+    
+  
